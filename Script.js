e3e88adf3df2277173c3848b667a61be4a3ba86f
Alex Luce <alexluce85@gmail.com>
2022-12-14 13:43:13 -0600

    starting javascript

---
+++ Script.js
@@ -0,0 +1,23 @@
+let time = moment();
+$("#currentDay").text(time.format("MMMM Do YYYY, h:mm:ss a"));
+function setTime() {
+  let timeInterval = setInterval(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  }, 1000);
+}
+setTime();
+$(".saveBtn").on("click", function () {
+  let text = $(this).siblings("textarea").val();
+  let time = $(this).siblings("div").text();
+  localStorage.setItem(time, text);
+  console.log(text);
+});
+$("#hour09").text(localStorage.getItem("09"));
+$("#hour10").text(localStorage.getItem("10"));
+$("#hour11").text(localStorage.getItem("11"));
+$("#hour12").text(localStorage.getItem("12"));
+$("#hour13").text(localStorage.getItem("13"));
+$("#hour14").text(localStorage.getItem("14"));
+$("#hour15").text(localStorage.getItem("15"));
+$("#hour16").text(localStorage.getItem("16"));
+$("#hour17").text(localStorage.getItem("17"));
